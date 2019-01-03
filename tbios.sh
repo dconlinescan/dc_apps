c26ae012a2be504e90c2483c2a3c4143b1c04fc7
tonny <tony@dcpt.online>
2019-01-03 15:47:11 +0800

    ios包

--- tbios.sh
+++ tbios.sh
@@ -5,13 +5,13 @@ cat ${config_file} |while read line
 do
 	name=`echo $line |sed 's/ //'`
 	echo $name
-	source_file="D:/dc_apps/tbios/${name}cp.ipa"
+	source_file="D:/gitwork/dc_apps/tbios/${name}cp.ipa"
 
 	cd "xy000"$line
 	git checkout dev
 	git pull origin dev
 	echo "拉取$name盘dev"
-	cp -r -f $source_file "D:/dc_apps/xy000$name"
+	cp -r -f $source_file "D:/gitwork/dc_apps/xy000$name"
 	echo "替换盘口ioS包"
 	git add -A
 	git commit -m "ios包 "
