651c5d40fd4868ed647bd805611a7c49cc4fc8ec
tonny <tony@dcpt.online>
2019-01-03 15:23:46 +0800

    ios包

---
+++ tbios.sh
@@ -0,0 +1,23 @@
+#!bash
+#同步ios的ipa包！
+config_file='./tbIOS.list'
+cat ${config_file} |while read line 
+do
+	name=`echo $line |sed 's/ //'`
+	echo $name
+	source_file="D:/app/tbios/${name}cp.ipa"
+
+	cd "xy000"$line
+	git checkout dev
+	git pull origin dev
+	echo "拉取$name盘dev"
+	cp -r -f $source_file "D:/app/dc_apps/xy000$name"
+	echo "替换盘口ioS包"
+	git add -A
+	git commit -m "ios包 "
+	git push origin dev
+	echo "xy000$name完成同步"
+	cd ../
+done
+
+sleep 3
